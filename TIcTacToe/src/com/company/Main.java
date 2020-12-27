7e6e825c23234822cd5e770b70534705f372770d
Devin Hadley <devinhadley4@gmail.com>
2020-12-26 17:05:54 -0800

    Uploading files....

---
+++ TIcTacToe/src/com/company/Main.java
@@ -0,0 +1,97 @@
+package com.company;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int current_player = 1;
+        boolean winner = false;
+        boolean remains = true;
+        boolean error = false;
+        int[][] board = {{0,0,0},{0,0,0},{0,0,0}}; // Setting Board
+        while(!winner && remains){
+            System.out.print("\033[H\033[2J");
+            System.out.flush();
+            if (error){
+                System.out.println("Slot taken or invalid, try again!");
+                error = false;
+            }
+            System.out.println("---------");
+            for (int i = 0; i < board.length; i++){ // Renders the board based off of the values in the 2d array board.
+                for (int j = 0; j < board[i].length; j++){ // 0 is blank, 1 is X, 2 is O.
+                    int current_val = board[i][j];
+                    switch (current_val){
+                        case 0:
+                            System.out.print(" | ");
+                            break;
+                        case 1:
+                            System.out.print(" X ");
+                            break;
+                        case 2:
+                            System.out.print(" O ");
+                            break;
+                    }
+                }
+                System.out.print("\n");
+                System.out.println("---------");
+            } // Rendering board
+            while(true){
+                System.out.println("Player "+current_player+" please enter your move.");
+                int gridX = scan.nextInt(); // index in the 2d array.
+                int gridY = scan.nextInt(); // Which 2d array.
+                if (gridY <= 3 && gridX <= 3 && board[gridY-1][gridX-1] == 0){
+                    board[gridY-1][gridX-1] = current_player;
+                    // Check the board for winner.
+                    for (int i = 0; i < board.length; i++){ // Checks for horizontal victories.
+                        int[] current_num = board[i];
+                        if(current_num[0] != 0 && (current_num[0] == current_num[1]) && (current_num[0] == current_num[2])){
+                            winner = true;
+                            System.out.print("Player "+current_player+" is the winner by horizontal allignment!");
+                            break;
+                        }
+                    }
+                    for (int i = 0; i < board.length; i++){
+                        int[] current_num = board[i];
+                        if (board[0][i] != 0 && (board[0][i] == board[1][i]) && board[0][i] == board[2][i]){ // Checks for vertical allignment.
+                            System.out.print("Player "+current_player+" is the winner by vertical allignment!");
+                            winner = true;
+                        }
+                    }
+                    // Checking diagonal allignment
+                    if(board[0][0] != 0 && board[0][0] == board[1][1] && board[0][0] == board[2][2]){
+                        System.out.print("Player "+current_player+" is the winner by diagonal allignment!");
+                        winner = true;
+                    } else if (board[0][2] != 0 && board[0][2] == board[1][1] && board[0][2] == board[2][0]){
+
+                        System.out.print("Player "+current_player+" is the winner by diagonal allignment!");
+                        winner = true;
+                    }
+                    remains = false;
+                    for (int i = 0; i < board.length; i++){ // Checks if any empty positions on the board exist.
+
+                        for(int j = 0; j < board[i].length; j++){
+
+                            if (board[i][j] == 0){
+                                remains= true;
+                            }
+                        }
+                    }
+                    if (!remains){
+                        System.out.print("Game has reached stalemate!");
+                    }
+                    if (current_player == 1){
+                        current_player = 2;
+                    } else {
+                        current_player = 1;
+                    }
+                } else {
+                    error = true;
+                }
+                break;
+            }
+        }
+    }
+}
